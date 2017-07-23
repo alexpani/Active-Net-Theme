7a73a5f3a1e80eea04b269f1eb02b84ba0f5a83a
Alessandro Pani <alessandro@MBPdiAlessandro-294.homenet.telecomitalia.it>
2017-07-23 15:41:52 +0200

    Active Net Genesis Theme
    Active Net è il nostro tema base per Genesis. E’ derivato dal Genesis
    Sample Theme. Include il pieno supporto per WooCommerce 3.x ed è
    predisposto per le estensioni Poppy.

---
+++ functions.php
@@ -0,0 +1,187 @@
+<?php
+/**
+ * Active Net
+ * @version 3.0
+ * @date 23/07/2017
+ * Based on Genesis Sample Child Theme
+ *
+ * @package Poppy Framework
+ * @author  Active Net
+ * @license GPL-2.0+
+ * @link    http://www.active-net.it
+ */
+
+//* Start the engine
+include_once( get_template_directory() . '/lib/init.php' );
+
+//* Setup Theme
+include_once( get_stylesheet_directory() . '/lib/theme-defaults.php' );
+
+//* Set Localization (do not remove)
+load_child_theme_textdomain( 'activenet', apply_filters( 'child_theme_textdomain', get_stylesheet_directory() . '/languages', 'activenet' ) );
+
+//* Add Image upload and Color select to WordPress Theme Customizer
+require_once( get_stylesheet_directory() . '/lib/customize.php' );
+
+//* Include Customizer CSS
+include_once( get_stylesheet_directory() . '/lib/output.php' );
+
+//* Child theme (do not remove)
+define( 'CHILD_THEME_NAME', 'Active Net' );
+define( 'CHILD_THEME_URL', 'http://www.active-net.it' );
+define( 'CHILD_THEME_VERSION', '1.6' );
+
+//* Start the WooCommerce Active Net Framework (only if WooCommerce is active)
+if ( in_array( 'woocommerce/woocommerce.php', apply_filters( 'active_plugins', get_option( 'active_plugins' ) ) ) ) {
+    include ('woocommerce/woocommerce.php');
+}
+
+//* Enqueue Scripts and Styles
+add_action( 'wp_enqueue_scripts', 'activenet_enqueue_scripts_styles' );
+function activenet_enqueue_scripts_styles() {
+
+	wp_enqueue_style( 'activenet-fonts', '//fonts.googleapis.com/css?family=Roboto:400,700,500|Montserrat:400,700|Poppins:400,500,700', array(), CHILD_THEME_VERSION );
+
+	//wp_enqueue_style( 'dashicons' );
+
+	wp_enqueue_style( 'font-awesome', CHILD_URL . '/fonts/font-awesome-4.7.0/css/font-awesome.min.css' );
+
+	wp_enqueue_script( 'poppy-responsive-menu', get_stylesheet_directory_uri() . '/js/responsive-menu.js', array( 'jquery' ), '1.0.0', true );
+	$output = array(
+		'mainMenu' => __( 'Menu', 'activenet' ),
+		'subMenu'  => __( 'Menu', 'activenet' ),
+	);
+	wp_localize_script( 'poppy-responsive-menu', 'genesisSampleL10n', $output );
+
+	wp_enqueue_style( 'custom-css-stylesheet', CHILD_URL . '/css/custom.css', array(), PARENT_THEME_VERSION );
+
+}
+
+//* Add HTML5 markup structure
+add_theme_support( 'html5', array( 'caption', 'comment-form', 'comment-list', 'gallery', 'search-form' ) );
+
+//* Add viewport meta tag for mobile browsers
+add_theme_support( 'genesis-responsive-viewport' );
+
+//* Add support for custom header
+add_theme_support( 'custom-header', array(
+	'width'           => 600,
+	'height'          => 150,
+	'header-selector' => '.site-title a',
+	'header-text'     => false,
+	'flex-height'     => true,
+) );
+
+//* Add support for custom background
+add_theme_support( 'custom-background' );
+
+//* Add support for after entry widget
+//add_theme_support( 'genesis-after-entry-widget-area' );
+
+//* Add support for 3-column footer widgets
+add_theme_support( 'genesis-footer-widgets', 3 );
+
+//* Add Image Sizes
+//add_image_size( 'featured-image', 720, 400, TRUE );
+
+//* Reposition the secondary navigation menu
+remove_action( 'genesis_after_header', 'genesis_do_subnav' );
+add_action( 'genesis_before_header', 'genesis_do_subnav', 5 );
+
+//* Reduce the secondary navigation menu to one level depth
+add_filter( 'wp_nav_menu_args', 'activenet_secondary_menu_args' );
+function activenet_secondary_menu_args( $args ) {
+	if ( 'secondary' != $args['theme_location'] ) {
+		return $args;
+	}
+	$args['depth'] = 1;
+	return $args;
+}
+
+//* Modify size of the Gravatar in the author box
+add_filter( 'genesis_author_box_gravatar_size', 'genesis_sample_author_box_gravatar' );
+function genesis_sample_author_box_gravatar( $size ) {
+	return 90;
+}
+
+//* Modify size of the Gravatar in the entry comments
+add_filter( 'genesis_comment_list_args', 'genesis_sample_comments_gravatar' );
+function genesis_sample_comments_gravatar( $args ) {
+	$args['avatar_size'] = 60;
+	return $args;
+}
+
+//* Add slider widget area
+genesis_register_sidebar( array(
+	'id'            => 'slider',
+	'name'          => __( 'Slider', 'activenet' ),
+	'description'   => __( 'Slider Area', 'activenet' ),
+) );
+
+add_action( 'genesis_after_header', 'slider_widget',20 );
+	function slider_widget() {
+	if ( is_front_page() )
+		genesis_widget_area( 'slider', array(
+			'before' => '<div class="slider widget-area"><div class="wrap">',
+			'after' => '</div></div>',
+	) );
+}
+
+//* Customize the credits
+add_filter( 'genesis_footer_creds_text', 'an_footer_creds_text' );
+function an_footer_creds_text() {
+	echo '<p>©2017 Active Net Framework · P.IVA 0000000 · Web Design by <b>Active Net</b></p>';
+}
+
+//* Add custom header area
+add_action( 'genesis_header', 'an_do_header' );
+function an_do_header() {
+	genesis_widget_area( 'header', array(
+		'before'	=> '<div class="header widget-area">',
+		'after'		=> '</div>',
+	) );
+}
+
+//* Remove Post Title from home
+add_action( 'get_header', 'an_remove_titles_home_page' );
+function an_remove_titles_home_page() {
+    if ( is_front_page() ) {
+        remove_action( 'genesis_entry_header', 'genesis_do_post_title' );
+    }
+}
+
+//* Customize the Wordpress login page
+add_action('login_head', 'an_custom_login_logo');
+function an_custom_login_logo() {
+	echo '<style type="text/css">
+	h1 a {background-image: url('.get_bloginfo('stylesheet_directory').'/images/logo.png) !important; width: 250px !important; height: 50px !important; background-size: 250px 50px !important; }
+	body.login {background: #0c3043;}
+	p#backtoblog {display: none;}
+	.login #nav a {color: #fff;}
+	</style>';
+}
+
+add_filter( 'login_headerurl', 'an_login_logo_url' );
+function an_login_logo_url() {
+return get_bloginfo( 'url' );
+}
+
+add_filter( 'login_headertitle', 'an_login_logo_url_title' );
+function an_login_logo_url_title() {
+return 'Active Net';
+}
+
+//* Customize the entry info in the entry header
+add_filter( 'genesis_post_info', 'an_post_info_filter' );
+function an_post_info_filter($post_info) {
+	$post_info = '<i class="fa fa-calendar" aria-hidden="true"></i> [post_date] | <i class="fa fa-user-o" aria-hidden="true"></i> <i>[post_author_posts_link]</i> [post_comments] [post_edit]';
+	return $post_info;
+}
+
+//* Customize the post meta function
+add_filter( 'genesis_post_meta', 'an_post_meta_filter' );
+function an_post_meta_filter($post_meta) {
+if ( !is_page() ) {
+	$post_meta = '[post_categories before="Categoria: "] [post_tags before="Etichetta: "]';
+	return $post_meta;
+}}
